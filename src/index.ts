f383776bbf5dcb492918d53485fe3b2a0f33bcec
Dennis Varona <altf4.personal@gmail.com>
2024-09-24 00:13:12 +0800

    added some todos

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ export const prisma = new PrismaClient()
 
 dotenv.config({ path: `.env` })
 
+// TODO: check every prisma query and mutation for userId if needed
+// TODO: create budget only if atleast income per hour is set ( check for any needed data )
+
 const app = express()
 
 // START OF MIDDLEWARES ---
